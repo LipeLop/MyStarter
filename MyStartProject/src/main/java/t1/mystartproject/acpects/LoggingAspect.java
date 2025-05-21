74705f27a5df37dd9faf268d77f45e7eaa5efde0
denda <den.dark.04@mail.ru>
2025-05-21 16:04:00 +0700

    Completed

---
+++ MyStartProject/src/main/java/t1/mystartproject/acpects/LoggingAspect.java
@@ -0,0 +1,44 @@
+package t1.mystartproject.acpects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import t1.mystartproject.properties.LoggingProperties;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggingAspect {
+
+    private final Map<String, Consumer<String>> logLevelMap = Map.of(
+            "debug", log::debug,
+            "info", log::info,
+            "warn", log::warn,
+            "error", log::error
+    );
+
+    private final LoggingProperties loggingProperties;
+
+    public LoggingAspect(LoggingProperties loggingProperties) {
+        this.loggingProperties = loggingProperties;
+    }
+
+    @Before("@annotation(t1.mystartproject.annotations.Logging)")
+    public void logging(JoinPoint joinPoint) {
+        if (!loggingProperties.isEnabled()) {
+            return;
+        }
+        logging(String.format("The method %s of class %s was called",
+                joinPoint.getSignature().getName(), joinPoint.getTarget().getClass().getName()), loggingProperties.getLevel());
+    }
+    private void logging(String message, String level) {
+        logLevelMap.getOrDefault(level.toLowerCase(), log::info).accept(message);
+    }
+
+
+}
---
+++ MyStartProject/src/main/java/t1/mystartproject/config/LoggingAutoConfiguration.java
@@ -0,0 +1,23 @@
+package t1.mystartproject.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import t1.mystartproject.acpects.LoggingAspect;
+import t1.mystartproject.annotations.Logging;
+import t1.mystartproject.properties.LoggingProperties;
+
+@ConditionalOnClass(LoggingAspect.class)
+@EnableConfigurationProperties(LoggingProperties.class)
+@Configuration
+public class LoggingAutoConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.aspect", name = "enabled", havingValue = "true", matchIfMissing = true)
+    public LoggingAspect logAspect(LoggingProperties properties) {
+        return new LoggingAspect(properties);
+    }
+}
